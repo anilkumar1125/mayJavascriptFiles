9544c6c5637b88d0b1a9d3eab25028bdb3f1cd49
Anil Kumar <anil.kumar440138@gmail.com>
2024-10-06 15:09:11 +0530

    amil

---
+++ day1_basic.js
@@ -0,0 +1,19 @@
+console.log("ABCD");
+
+// const let
+const name1 = 123456;
+let name2 = false;
+
+// name1 = "abcdefgh";
+// name2 = "ijklmnop";
+
+console.log(name1);
+console.log(name2);
+
+
+
+
+
+
+
+
